fa28a13ec72ad9354523aa3be44e86d33cf1490d
Alex Villa <alex.villa.v@gmail.com>
2016-10-20 11:49:07 -0300

    add maps redirect

--- js/view/CalendarView.js
+++ js/view/CalendarView.js
@@ -1,7 +1,6 @@
 var CalendarView = function () {
     this.initialize = function () {
         this.el = $('<div/>');
-        //this.el.ready(this.loadEvents);
     };
 
     this.render = function () {
@@ -10,20 +9,6 @@ var CalendarView = function () {
     };
 
     this.loadEvents = function () {
-//        var JsonEvents = JSON.parse(events);
-//        var JsonEvents = JsonEvents.items;
-//        var cal = new Array({});
-//        for(var i = 0; i < JsonEvents.length; i++){
-//            if(JsonEvents[i].status==="confirmed")
-//                {
-//                    cal[i] = "elemento "+i;
-//                    cal[i].location = JsonEvents[i]['location'];
-//                    cal[i].endDate = JsonEvents[i]['end']['datetime'];
-//                    cal[i].startDate = JsonEvents[i]['start']['datetime'];
-//                    cal[i].location = JsonEvents[i]['summary'];
-//                }
-//        }
-//        $('.calendar-content').html(CalendarView.liTemplate());
         return this;
     };
 
@@ -31,4 +16,3 @@ var CalendarView = function () {
 };
 
 CalendarView.template = Handlebars.compile($("#calendar-tpl").html());
-//CalendarView.liTemplate = Handlebars.compile($("#calendar-li-tpl").html());
